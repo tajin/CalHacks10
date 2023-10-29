a60603f303b7e829dd59ea1ccaca56a18bdcbe29
Angel Perez <angelalexisperez@yahoo.com>
2023-10-29 02:06:59 -0700

    Created second page and improved first page

--- CalHacks10/CalHacks10.py
+++ CalHacks10/CalHacks10.py
@@ -2,15 +2,38 @@ import reflex as rx
 
 class FormState(rx.State):
 
-    form_data: dict ={}
+    form_data: dict = {}
 
     def handle_submit(self, form_data: dict):
         """Handle the form submit."""
         self.form_data = form_data
+        return [
+            rx.set_value(field_id, "")
+            for field_id in form_data
+        ]
+
+def navbar():
+    return rx.hstack(
+        rx.hstack(
+            rx.image(src="/budget-line-icon-logo-illustration-free-vector.jpg", width="50px"),
+            rx.heading("FuckThis"),
+        ),
+        rx.spacer(),
+        rx.menu(
+            rx.menu_button("Menu"),
+        ),
+        position="fixed",
+        width="100%",
+        top="0px",
+        z_index="5",
+        padding_x="2em"
+    )
+
 
 
 def index():
     return rx.vstack(
+        navbar(),
         rx.image(
             src="/image-2.svg", width="250px", height="350"),
         rx.form(
@@ -29,6 +52,29 @@ def index():
         ),
         rx.divider(),
     )
+
+def budget():
+    return rx.vstack(
+        rx.form(
+            rx.vstack(
+                rx.input(
+                    placeholder="First Name",
+                    id="first_name",
+                ),
+                rx.input(
+                    placeholder="Last Name", id="last_name"
+                ),
+                rx.button("Submit", type_="submit"),
+            ),
+            on_submit=FormState.handle_submit,
+        ),
+        rx.divider(),
+        rx.heading("Results"),
+        rx.text(FormState.form_data.to_string()),
+    )
+
+
 app = rx.App()
 app.add_page(index)
+app.add_page(budget, route='/budget_page')
 app.compile()
--- rxconfig.py
+++ rxconfig.py
@@ -4,3 +4,4 @@ config = rx.Config(
     app_name="CalHacks10",
     db_url = "postgresql://tajin_user:cEPxiKzEJsW0n9tU-iDiXQ@calhacks-cockroachdb-10735.6wr.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
 )
+
